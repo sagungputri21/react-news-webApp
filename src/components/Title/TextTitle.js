cb0762672925489b64c8fc02b149776c7c4e9cca
Sagung Putri Nariswari <sagungputri1530@gmail.com>
2022-10-21 23:18:50 +0700

    create new pages and adding route
    - created new page for covid 19 and programming page
    - add new routes for those pages

---
+++ src/components/Title/TextTitle.js
@@ -0,0 +1,7 @@
+const TextTitle = () => {
+    return (
+        <div>for title</div>
+    )
+}
+
+export default TextTitle;
--- src/pages/Programming.js
+++ src/pages/Programming.js
@@ -1,20 +1,17 @@
 import React, { useState, useEffect } from "react";
 import client from "../api/baseApi";
 import NewsCard from "../components/Cards/NewsCard";
-import SavedNewsCard from "../components/Cards/SavedNewsCard";
 import Container from 'react-bootstrap/Container';
 
-const Test = () => {
-  //atur last month ==> cek lagi besok
+const ProgrammingPage = () => {
   const current = new Date();
   const date = new Date(current.setMonth(current.getMonth() - 1)).toISOString();
-  console.log(date)
   const [news, setNews] = useState([]);
 
   const newsData = async () => {
     try {
       const data = await client.get(
-        `everything?q=covid&from=${date}&sortBy=publishedAt`
+        `everything?q="programming"&from=${date}&sortBy=popularity`
       );
       setNews(data.data.articles);
     } catch (err) {
@@ -29,7 +26,6 @@ const Test = () => {
   console.log(news);
 
   return (
-    // <Container>
     <div className="container-xxl">
       <div className="card-section">
         {news?.slice(0, 10).map((data) => (
@@ -44,31 +40,8 @@ const Test = () => {
           />
         ))}
       </div>
-      <div className="saved-layout">
-      {news?.slice(0, 5).map((data) => (
-          <SavedNewsCard 
-            imageURL={data.urlToImage}
-            name={data.source.name}
-            title={data.title}
-            author={data.author}
-            path={data.url}
-            desc={data.description}
-          />
-        ))}
-        {news?.slice(6, 10).map((data) => (
-          <SavedNewsCard 
-            imageURL={data.urlToImage}
-            name={data.source.name}
-            title={data.title}
-            author={data.author}
-            path={data.url}
-            desc={data.description}
-          />
-        ))}
     </div>
-    </div>
-    // </Container>
   );
 };
 
-export default Test;
+export default ProgrammingPage;
